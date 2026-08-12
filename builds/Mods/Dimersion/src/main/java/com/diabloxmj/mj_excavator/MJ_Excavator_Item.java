ef5e9cf309e1286dabb9395b7a14403de2a78252
DIABLOxMJ <DIABLOxMJ@users.noreply.github.com>
2026-08-12 15:49:36 +0200

    Moved

--- builds/Mods/Dimersion/src/main/java/com/diabloxmj/mj_excavator/MJ_Excavator_Item.java
+++
@@ -1,5 +0,0 @@
-package com.diabloxmj.mj_excavator;
-
-public interface MJ_Excavator_Item {
-    // Cette interface sert de marqueur. Si un item l'implémente, le Mixin saura que c'est un excavateur !
-}
